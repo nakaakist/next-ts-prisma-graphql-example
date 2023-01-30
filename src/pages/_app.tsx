f9d68d86e48b778556c56e58aac32d050b930352
Nariyuki Saito <toshincompos@gmail.com>
2023-01-31 01:42:35 +0900

    Change from app dir to pages dir

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+import { Inter } from "@next/font/google";
+import type { AppProps } from "next/app";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} className={inter.className} />;
+}
