200d0786974484740c0f49ef61c17674c78ae8f9
Nariyuki Saito <toshincompos@gmail.com>
2023-02-06 02:13:14 +0900

    Fix resolver for query optimization

--- src/lib/resolvers.ts
+++ src/lib/resolvers.ts
@@ -3,8 +3,25 @@ import prisma from "./prisma";
 
 export const resolvers = {
   Query: {
+    // TODO: fix type error.
+    // This error is due to the return type of findMany() does not include topics,
+    // but Prisma correctly returns topics when needed.
+    // @ts-expect-error
     posts: async () => {
-      return await prisma.post.findMany({ include: { topics: true } });
+      return await prisma.post.findMany();
+    },
+  },
+  Post: {
+    topics: async (parent) => {
+      return (
+        (await prisma.post
+          .findUnique({
+            where: {
+              id: parent.id,
+            },
+          })
+          .topics()) ?? []
+      );
     },
   },
 } satisfies Resolvers;
